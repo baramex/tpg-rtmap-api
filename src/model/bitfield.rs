edbb26ad51348b3445848950e17cc51655c0c1bf
baramex <contact@baramex.me>
2023-10-14 20:08:41 +0200

    [TASK] Add query to database struct and add create table to bitfield

--- src/model/bitfield.rs
+++ src/model/bitfield.rs
@@ -1,8 +1,19 @@
 use serde::Serialize;
-use sqlx::FromRow;
+use sqlx::{FromRow, Error, postgres::PgQueryResult};
+
+use crate::repository::database::Database;
 
 #[derive(Serialize, FromRow, Debug)]
 pub struct Bitfield {
     pub id: u32,
     pub days: String,
+}
+
+impl Bitfield {
+    pub async fn create_table(database: Database) -> Result<PgQueryResult, Error> {
+        database.query("CREATE TABLE IF NOT EXISTS bitfield (
+            id INTEGER PRIMARY KEY,
+            days VARCHAR NOT NULL
+        )").await
+    }
 }
